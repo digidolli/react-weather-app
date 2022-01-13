9bd8c28aa625a1dec9f6ebde5f95dc050dbe5538
Digidolli <iamsadekaye@gmail.com>
2022-01-13 19:00:20 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+      <button className="btn btn-primary"> hello</button>
       <h1>
         React Weather App
       </h1>
       <footer>
-        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank"> open-sourced on Github </a> 
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
       </footer>
+    </div>
     </div>
   );
 }
