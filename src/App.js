ebd6576206165de308802dc95fae88018de51a36
Digidolli <iamsadekaye@gmail.com>
2022-01-13 18:33:03 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>
+        React Weather App
+      </h1>
+      <footer>
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank"> open-sourced on Github </a> 
+      </footer>
     </div>
   );
 }
