7f806fbf8baf1bcdd5941bdc689402910609684d
Digidolli <iamsadekaye@gmail.com>
2022-01-13 19:16:50 +0000

    Added HTML elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <button className="btn btn-primary"> hello</button>
-      <h1>
-        React Weather App
-      </h1>
+      
+      <Weather />
       <footer>
         This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
       </footer>
