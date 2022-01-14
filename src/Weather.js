5d53bc2825a35ed1469fb28ca9a7d79efea75a58
Digidolli <iamsadekaye@gmail.com>
2022-01-14 23:06:41 +0000

    Updated icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import TidyDate from "./TidyDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props){
-    
+    const [city, setCity] = useState(props.defaultCity)
     const [weatherData, setWeatherData] = useState ({ready: false});
     function handleResponse(response) {
         setWeatherData({
@@ -15,49 +15,44 @@ export default function Weather(props){
             date: new Date(response.data.dt * 1000),
             humidity: response.data.main.humidity,
             details: response.data.weather[0].description,
-            icon: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             iconAlt: "hello",
 
         });
     };
 
+    function search (){
+        const apiKey = "faa5a241d5c428a14c3417782b47bc2d";  
+        let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiURL).then(handleResponse);
+
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        search(city);
+
+    }
+
+    function handleCityChange(event){
+        setCity(event.target.value);
+        
+    }
 
     if(weatherData.ready){
         return(
-        <div className="Weather">
-            <div className="weather-app">
-                <form id="search-bar" >
-                    <input  type="search" name="City Search" placeholder="Please enter a city..."  autoComplete="off" id="city-search" />
-                    <input type="submit" value="🔎" id="search-city-button" />
-                </form>
-                <div className="currentForecast row">
-                    <div className="cityHeading">
-                        <h1 className="place text-capitalize" id="place"> {weatherData.city}</h1>
-                        <h1 className="day text-capitalize" > <TidyDate date={weatherData.date} /><span id="day"> </span></h1>
-                    </div>
-                    <div className="col-6 firstCol">
-                        <img src={weatherData.icon} alt={weatherData.iconAlt} className="icon-cloudy" id="icon" />
-                    </div>
-                    <div className="col-6 secondCol">
-                        <ul>
-                            <li className="currentTemp" id="temp"> {Math.round(weatherData.temp)}º
-                            </li>
-                        </ul>
-                    </div>
-                    <h1 className="weatherDetails text-capitalize" id="weatherDetails">{weatherData.details}</h1>
-                    <p>
-                    <strong>Humidity: <span id="humidity">{weatherData.humidity}</span> </strong> <strong>%</strong>
-                    <strong>| Wind: <span id="wind">{weatherData.wind}</span> Km/H</strong>
-                    </p>
-                    <div className="upcomingForecast row " id="upcomingForecast"></div>
-                    </div>
-                </div>
-            </div>   
-    );
-    } else {
-            const apiKey = "faa5a241d5c428a14c3417782b47bc2d";  
-            let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-            axios.get(apiURL).then(handleResponse);
+            <div className="Weather">
+                <div className="weather-app">
+                    <form id="search-bar" onSubmit={handleSubmit}>
+                        <input  type="search" name="City Search" placeholder="Please enter a city..."  autoComplete="off" id="city-search" onChange={handleCityChange} />
+                        <input type="submit" value="🔎" id="search-city-button" />
+                    </form>
+                    <WeatherInfo data={weatherData} /> 
+                </div>   
+            </div>  
+        );
+        } else {
+            search();
             return ("loading...");
     }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import TidyDate from "./TidyDate";
+
+export default function WeatherInfo (props){
+return(
+    <div className="WeatherInfo">
+        <div className="currentForecast row">
+            <div className="cityHeading">
+                <h1 className="place text-capitalize" id="place"> {props.data.city}</h1>
+                <h1 className="day text-capitalize" > <TidyDate date={props.data.date} /><span id="day"> </span></h1>
+            </div>
+            <div className="col-6 firstCol">
+                <img src={props.data.icon} alt={props.data.iconAlt} className="icon-cloudy" id="icon" />
+            </div>
+            <div className="col-6 secondCol">
+                <ul>
+                    <li className="currentTemp" id="temp"> {Math.round(props.data.temp)}º
+                    </li>
+                </ul>
+            </div>
+            <h1 className="weatherDetails text-capitalize" id="weatherDetails">{props.data.details}</h1>
+            <p>
+                <strong>Humidity: <span id="humidity">{props.data.humidity}</span> </strong> <strong>%</strong>
+                <strong>| Wind: <span id="wind">{props.data.wind}</span> Km/H</strong>
+            </p>
+        </div>
+    </div>
+    
+    );
+};
